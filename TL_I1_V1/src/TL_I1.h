e405c7164897edc548e5f71f82fb78dd85a4dfea
ekul34 <s3509045@student.rmit.edu.au>
2019-09-29 20:42:59 +1000

    daySequence() cases completed Enums now follow "day1 day2 ... dayN" format

--- TL_I1_V1/src/TL_I1.h
+++ TL_I1_V1/src/TL_I1.h
@@ -8,8 +8,8 @@
 #ifndef TL_I1_H_
 #define TL_I1_H_
 
-enum lightState     {red, yellow, green, off, disabled};
-enum sequenceState  {initial, foo, bar};
+enum lightState     {red, yellow, green, off, disabled, flashing};
+enum sequenceState  {initial, day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12,};
 
 
 struct light
--- TL_I1_V1/src/TL_Threads.c
+++ TL_I1_V1/src/TL_Threads.c
@@ -13,28 +13,64 @@ void daySequence(void)
 
     switch(intersection.seqState)
 
-    {
-        case initial: setState(red,   red,   red,   red,      red,   red,   red,   red,
-                               red,   red,   red,   red,      red,   red,   red,   red);
-        break;
-        case 1:     setState(yellow,yellow,yellow,yellow,   yellow,yellow,yellow,yellow,
-                               yellow,yellow,yellow,yellow,   yellow,yellow,yellow,yellow);
-        break;
-        case 2:     setState(yellow,yellow,yellow,yellow,   yellow,yellow,yellow,yellow,
-                               yellow,yellow,yellow,yellow,   yellow,yellow,yellow,yellow);
-        break;
+    {   //           North                                       South
+        //           East                                        West
+        //           Straight    Left    Right    Pedestrian     Straight    Left    Right    Pedestrian
+
+        // Safe start
+        case initial:
+            setState(red,        red,    red,     red,           red,        red,    red,     red,
+                     red,        red,    red,     red,           red,        red,    red,     red);      intersection.seqState = day1;  break;
+        // NS start
+        case day1:
+            setState(red,        red,    green,   red,           red,        red,    green,   red,
+                     red,        green,  red,     red,           red,        green,  red,     red);      intersection.seqState = day2;  break;
+        case day2:
+            setState(red,        red,    yellow,  red,           red,        red,    yellow,  red,
+                     red,        yellow, red,     red,           red,        yellow, red,     red);      intersection.seqState = day3;  break;
+        case day3:
+            setState(red,        red,    off,     red,           red,        red,    off,     red,
+                     red,        red,    red,     green,         red,        red,    red,     green);    intersection.seqState = day4;  break;
+        case day4:
+            setState(green,      green,  off,     red,           green,      green,  off,     red,
+                     red,        red,    red,     green,         red,        red,    red,     green);    intersection.seqState = day5;  break;
+        case day5:
+            setState(yellow,     yellow, off,     red,           yellow,     yellow, off,     red,
+                     red,        red,    red,     flashing,      red,        red,    red,     flashing); intersection.seqState = day6;  break;
+        case day6:
+            setState(red,        red,    red,     red,           red,        red,    red,     red,
+                     red,        red,    red,     red,           red,        red,    red,     red);      intersection.seqState = day7;  break;
+        // EW start
+        case day7:
+            setState(red,        green,  red,     red,           red,        green,  red,     red,
+                     red,        red,    green,   red,           red,        red,    green,   red);      intersection.seqState = day8;  break;
+        case day8:
+            setState(red,        yellow, red,     red,           red,        yellow, red,     red,
+                     red,        red,    yellow,  red,           red,        red,    yellow,  red);      intersection.seqState = day9;  break;
+        case day9:
+            setState(red,        red,    red,     green,         red,        red,    red,     green,
+                     red,        red,    off,     red,           red,        red,    off,     red);      intersection.seqState = day10; break;
+        case day10:
+            setState(red,        red,    red,     green,         red,        red,    red,     green,
+                     green,      green,  off,     red,           green,      green,  off,     red);      intersection.seqState = day11; break;
+        case day11:
+            setState(red,        red,    red,     green,         red,        red,    red,     green,
+                     green,      green,  off,     red,           green,      green,  off,     red);      intersection.seqState = day12; break;
+        case day12:
+            setState(red,        red,    red,     red,           red,        red,    red,     red,
+                     red,        red,    red,     red,           red,        red,    red,     red);      intersection.seqState = day1; break;
+
         default:      intersection.seqState = initial;
     }
 };
 
-void nightSequence(void)
-{
-    switch(intersection.seqState)
-    {
-
-        case initial:
-        break;
-    }
-
-};
+//void nightSequence(void)
+//{
+//    switch(intersection.seqState)
+//    {
+//
+//        case initial:
+//        break;
+//    }
+//};
 
